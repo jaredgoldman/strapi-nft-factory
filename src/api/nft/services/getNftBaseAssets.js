663db220a1525e16d7d76088187942ab6031d12d
jaredgoldman <jaredgoldmandrums@gmail.com>
2022-02-07 17:14:51 -0500

    remove reference to handle layer directory

--- src/api/nft/services/getNftBaseAssets.js
+++ src/api/nft/services/getNftBaseAssets.js
@@ -96,7 +96,6 @@ const getNftBaseAssets = async () => {
 
 module.exports = async () => {
   try {
-    handleLayerDirCreated()
     await getNftBaseAssets()
   } catch (error) {
     console.log(error)
