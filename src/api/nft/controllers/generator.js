3dcca831efcc2a15eb0ab5fd38ea2b225934e6d0
jaredgoldman <jaredgoldmandrums@gmail.com>
2022-03-08 17:46:00 -0500

    update split

--- src/api/nft/controllers/generator.js
+++ src/api/nft/controllers/generator.js
@@ -82,7 +82,7 @@ const uploadAndMintProcess = async (config, metadata) => {
     console.log(err)
   })
 
-  console.log(nfts[0].split("-"))
+  console.log(nfts[0].split("-")[1].replace(".png", ""))
   nfts.sort((a, b) => {
     return (
       Number(a.slice(8).replace(".png", "")) -
